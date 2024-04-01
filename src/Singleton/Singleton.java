75b3a6208c268832f502b3ce0a671ccdbf961747
Ranjith Reddy N <ranjeethnivas8@gmail.com>
2024-03-31 20:11:37 -0400

    LLD-DesigPatterns-Singleton

---
+++ src/Singleton/Singleton.java
@@ -0,0 +1,17 @@
+package Singleton;
+
+@SuppressWarnings("ALL")
+public class Singleton {
+    private static Singleton singleton;
+
+    public static Singleton getInstance(){
+        if (singleton == null) {
+            synchronized (Singleton.class){
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
